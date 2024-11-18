9769b40e779e01e0b7d88f99c238804d175ebb7c
jen807 <80chill@naver.com>
2024-11-18 17:44:24 +0900

    fix size again

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -166,7 +166,7 @@ const RightCon = styled.div`
 
   @media screen and (max-width: 440px) {
     width: 100%;
-    height: 350px;
+    height: 300px;
     position: relative;
     top: -3vh;
     left: 0;
